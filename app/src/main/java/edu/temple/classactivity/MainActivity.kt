22c6d6dd14a15f3517c412e10d1ba2805d572252
depst-10538 <tur12015@temple.edu>
2025-09-09 15:20:30 -0400

    Added name entry with a submit button

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,6 +1,11 @@
 package edu.temple.classactivity
 
+import android.graphics.Color
+import android.hardware.biometrics.PromptContentItemPlainText
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +15,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.messageTextView)
+        val editText = findViewById<EditText>(R.id.nameEntry)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            if(editText.text.isEmpty()){
+                textView.text = "Please enter your name."
+                textView.setTextColor(Color.parseColor("#000000"))
+            }
+            else {
+                textView.text = "Hello, " + editText.text + "! Welcome."
+                editText.setText("")
+                textView.setTextColor(Color.parseColor("#FF8000"))
+            }
+        }
     }
 }
